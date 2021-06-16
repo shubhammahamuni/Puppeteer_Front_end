d5d8b5c08bc1b11b36af6026949dbed695a0d9d1
shubham mahamuni <shubham.mahamuni.902@mail.com>
2021-06-16 12:26:15 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import LikesForPost  from "./Component/likes";
 import './App.css';
 
 function App() {
+  const[likes , setlikes] =useState(); 
+
+  // useEffect(async ()=>{
+  //     //let a =await fetch("/likes").then((obj)=>{ return obj.json()}).then((actual) =>{console.log(actual)});
+  //     const res = await fetch("/likes");
+  //     const data = await res.json();
+  //     //console.log(data);
+  //     setlikes(data);
+  // },[])
+  const[url_input_field ,setinputfield ]  = useState({url:""});
+  const handlechange = (e)=>{
+    setinputfield({url: e.target.value});
+  }
+
+   const handleclick =async ()=>{
+
+      if(url_input_field.url){
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(url_input_field)
+          };
+         await fetch('/find', requestOptions)
+          .then(response => response.json())
+          .then(data => setlikes(data));
+      }
+      else{
+          alert("yay!!!");
+      }
+
+  }
+
+  //console.log(likes);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputtagbox" >
+        <input value={url_input_field.url} placeholder="Enter Public url" onChange={handlechange}></input>
+     
+      <button id="btn" onClick ={handleclick}>Submit</button>
+      </div>
+
+      <LikesForPost likes={likes}/>
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/Component/likes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Likes_of_Post({likes}) {
+    return (
+        <div>
+            <h1>Likes :  {likes}</h1>
+        </div>
+    )
+}
+
+export  default Likes_of_Post;
